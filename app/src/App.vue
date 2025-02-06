517229d3b7e1b48e6748a3a6a5c92a50f91f9658
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2025-02-06 14:03:51 -0500

    fds;,hkjhfjlhjfdslkiFJS

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,5 @@
-<script setup></script>
 <template>
-  <div>
+  <div class="bg-ALDIorange">
     <div class="navbar flex justify-between w-[100%] bg-ALDIdarkblue static">
       <figure>
         <img
@@ -9,16 +8,16 @@
           alt="ALDIs"
         />
       </figure>
-      <h1 class="">Hello</h1>
+      <h1 class="">Shop at ALDIs!</h1>
     </div>
     <ul class="steps">
-      <li data-content="?" class="step step-ALDIorange">Step 1</li>
-      <li data-content="!" class="step">Step 2</li>
-      <li data-content="✓" class="step">Step 3</li>
-      <li data-content="✕" class="step">Step 4</li>
-      <li data-content="★" class="step">Step 5</li>
-      <li data-content="" class="step">Step 6</li>
-      <li data-content="●" class="step">Step 7</li>
+      <li data-content="★" class="step step-ALDIorange">Step 1</li>
+      <li data-content="?" class="step">Step 2</li>
+      <li data-content="?" class="step">Step 3</li>
+      <li data-content="?" class="step">Step 4</li>
     </ul>
   </div>
 </template>
+<script setup>
+import { items } from './array.js'
+</script>
---
+++ app/src/array.js
@@ -0,0 +1,74 @@
+export const items = [
+  {
+    name: 'Popcorn',
+    ALDI_meter: 6,
+    price: 4.99,
+    image_link: 'app/public/popcorn.png',
+  },
+  {
+    name: 'Cheddar',
+    ALDI_meter: 2,
+    price: 1.99,
+    image_link: 'app/public/cheddar.png',
+  },
+  {
+    name: 'Quiche',
+    ALDI_meter: 6,
+    price: 4.99,
+    image_link: 'app/public/quiche.png',
+  },
+  {
+    name: 'Chicken',
+    ALDI_meter: 5,
+    price: 6.99,
+    image_link: 'app/public/chicken.png',
+  },
+  {
+    name: 'Peanut butter',
+    ALDI_meter: 4,
+    price: 3.49,
+    image_link: 'app/public/peanutbutter.png',
+  },
+  {
+    name: 'Milk chocolate',
+    ALDI_meter: 4,
+    price: 2.99,
+    image_link: 'app/public/milkchocolate.png',
+  },
+  {
+    name: 'Motorcycle',
+    ALDI_meter: 9,
+    price: 399.99,
+    image_link: 'app/public/motorcycle.png',
+  },
+  {
+    name: 'Penthouse',
+    ALDI_meter: 10,
+    price: 899.99,
+    image_link: 'app/public/penthouse.png',
+  },
+  {
+    name: 'Bomb',
+    ALDI_meter: 9,
+    price: 34.99,
+    image_link: 'app/public/bomb.png',
+  },
+  {
+    name: 'Chick-fil-A',
+    ALDI_meter: 6,
+    price: 9.99,
+    image_link: 'app/public/chickfila.png',
+  },
+  {
+    name: 'Alvin from Alvin and the Chipmunks',
+    ALDI_meter: 7,
+    price: 19.99,
+    image_link: 'app/public/alvin.png',
+  },
+  {
+    name: 'Gas tank',
+    ALDI_meter: 10,
+    price: 40.99,
+    image_link: 'app/public/gastank.png',
+  },
+]
